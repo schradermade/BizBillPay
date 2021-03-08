6fc99ea3ca4374a319e222fc3cec569b9cbf5b3a
Nathan Schrader <schradermade@Nathans-MBP.netgear.com>
2021-03-08 13:25:13 -0800

    add state

--- src/App.js
+++ src/App.js
@@ -2,3 +2,19 @@ import React, { Component } from 'react';
 import { Table, Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { } from '@fortawesome/free-solid-svg-icons';
+
+class App extends Component {
+  state = {
+    isLoading : false,
+    bills: [
+      {
+        'id' : '1',
+        'Bill' : 'Newberg Water',
+        'BillLink' : 'https://www.newbergoregon.gov/finance/page/how-pay-your-water-bill',
+        'Paid' : false
+      }
+    ]
+  }
+
+  
+}
